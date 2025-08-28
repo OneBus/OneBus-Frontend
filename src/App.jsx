3003840a7178bae75d338ff8addb322200e4eff7
GuiFrancini <guifo2004@gmail.com>
2025-08-28 15:40:35 -0300

    commit teste
    comit teste do github desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Sidebar from './components/Sidebar/Sidebar';
 import Header from './components/Header/Header';
-import AppRoutes from './routes/router'; 
+import Login from './pages/Login/Login';
+import AppRoutes from './routes/router';
+import PrivateRoute from './routes/PrivateRoute.jsx'; // jsx inves de js
 
-{/*seção de importação*/}
-
-
-
-function App() {
+const DashboardLayout = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
+  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
 
   return (
+    <div style={{ display: 'flex' }}>
+      <Sidebar isOpen={isSidebarOpen} />
+      <div style={{ flex: 1, marginLeft: isSidebarOpen ? '250px' : '0', transition: 'margin-left 0.3s ease-in-out' }}>
+        <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+        <AppRoutes />
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
     <Router>
-      <div style={{ display: 'flex'}}>
-        <Sidebar isOpen= {isSidebarOpen} />
-
-        <div style={{
-          flex:1,
-          marginLeft: isSidebarOpen ? '250px' : '0',
-          transition: 'margin-left 0.3s ease-in-out',
-        }}>
-
-          <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-   <AppRoutes/>
-      </div>
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={
+            <PrivateRoute>
+              <DashboardLayout />
+            </PrivateRoute>
+          } 
+        />
+      </Routes>
     </Router>
   );
 }
+
 
 export default App;
